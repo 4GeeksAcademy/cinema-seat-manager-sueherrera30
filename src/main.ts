0cec10bd6bde3fcd7beaf4b08c03b90a52578089
sue herrera <sueherrera30@gmail.com>
2026-08-15 03:38:23 +0000

    Feat: crear función crear sala de cine donde devuelve matriz con hacientos vacios como default

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,22 @@ if (typeof document !== "undefined") {
 }
 
 console.log("Hello from src/main.ts");
-const getSitStatus = (): string => {
- return "hello testing function!";
+const crearSalaDeCine = (filas: number, columnas: number): number[][] => {
+  const salaDeCine: number[][] = [];
+  const libre = 0;
+  const ocupado = 1;
+
+  for(let fila:number = 0; fila < filas; fila++) {
+     let filaActual: number[] = [];
+     for(let columna:number = 0; columna < columnas; columna++){
+      filaActual.push(libre);
+     }
+     salaDeCine.push(filaActual)
+  };
+  return salaDeCine;
 };
 
-console.log(getSitStatus());
-
+console.log(crearSalaDeCine(8,10));
+console.log(crearSalaDeCine(5,20));
+console.log(crearSalaDeCine(10,5));
 export {};
